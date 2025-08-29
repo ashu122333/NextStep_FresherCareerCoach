933a378cbed1f323a942df6bd8da9e7556d3b5b5
ashu122333 <ashu.study.03@gamil.com>
2025-08-29 18:37:04 +0530

    added authentication using clerk tool

--- app/page.js
+++ app/page.js
@@ -4,8 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1>made by ANSU GUPTA!!!</h1>
-      <Button variant="default">Get Started</Button>
+      
     </div>
   )
 }
--- components/header.jsx
+++ components/header.jsx
@@ -1,11 +1,119 @@
 import React from "react";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { ChevronDown, FileText, GraduationCapIcon,PictureInPicture2 ,LayoutDashboard, PenBox, StarsIcon } from "lucide-react";
+
+
+
 
 function Header() {
   return (
-    <header className="bg-muted/50 py-12">
-      <div className="container mx-auto px-4 text-center text-gray-200">
-        <h1 className="text-3xl font-bold">Welcome to my Project</h1>
-      </div>
+    <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
+        
+        <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+            <Link href="/">
+
+            <span className="flex items-center gap-2">
+                <Image src="/logo.png" alt="Logo" width={100} height={100} 
+                className="h-12 py-1 w-auto object-contain"/>
+                <span className="font-bold text-xl text-primary">NextStep</span>
+            </span>
+
+          </Link>
+
+            <div className="flex items-center space-x-2 md:space-x-4">
+
+            <SignedIn>
+              <Link href="/dashboard">
+                <Button variant="outline">
+                    <LayoutDashboard className="h-4 w-4"/>
+                     <span className="hidden md:block"> Market Analysis </span>  
+                </Button>
+              </Link>
+
+
+            <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Button variant="default">
+                    <StarsIcon className="h-4 w-4"/>
+                     <span className="hidden md:block"> Tools </span>  
+                     <ChevronDown className="h-4 w-4" />
+                </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent>
+
+                <DropdownMenuItem>
+                    <Link href={"/resume"} className="flex items-center gap-2">
+                        <FileText className="h-4 w-4"/>
+                        <span> Build Resume </span>  
+                    </Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                    <Link href={"/cover-letter"} className="flex items-center gap-2">
+                        <PenBox className="h-4 w-4"/>
+                        <span> Build Cover Letter </span>  
+                    </Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                    <Link href={"/mock-test"} className="flex items-center gap-2">
+                        <GraduationCapIcon className="h-4 w-4"/>
+                        <span> Mock Test </span>  
+                    </Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                    <Link href={"/mock-interview"} className="flex items-center gap-2">
+                        <PictureInPicture2 className="h-4 w-4"/>
+                        <span>Mock Interview</span>  
+                    </Link>
+                </DropdownMenuItem>
+
+            </DropdownMenuContent>
+            </DropdownMenu>
+
+            </SignedIn>
+
+
+            <SignedOut>
+                <SignInButton>
+                    <Button variant="outline">Sign In</Button>
+                </SignInButton>
+            </SignedOut>
+
+            <SignedIn>
+                <UserButton  appearance={{
+                    elements: {avatarBox:"w-10 h-10",
+                            userButtonPopoverCard:"shadow-xl",
+                            userPreviewMainIdentifier:"font-semibold",
+                    }}} afterSignOutUrl="/"/>
+            </SignedIn>
+
+            </div>
+        </nav>
+
+
+
+        
+        
     </header>
   );
 }
